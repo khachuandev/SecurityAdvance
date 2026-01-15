fdec5659c12c49c90866675f029d9f576c32d94b
khachuandev <huan12a2002@gmail.com>
2026-01-15 09:00:37 +0700

    init project

---
+++ src/main/java/com/example/SecurityAdvance/SecurityAdvanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.SecurityAdvance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAdvanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityAdvanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SecurityAdvance/SecurityAdvanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SecurityAdvance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityAdvanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
